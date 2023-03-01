e3b08d6d865d4de3c07d357ece9cd834408df8a8
OnlineNomad <singularity123321@gmail.com>
2023-03-01 16:50:59 +0400

    Add task one

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello World !")
+const user = [{name: 'Temo', age: 25}, {name: 'Lasha', age: 21}, {name: 'Ana', age: 28}];
+
+function findYoungest(user) {
+    user.forEach(function(item){
+        if (item.age <= 21){
+            console.log(`${item.name}`)
+        }
+    })
+}
+findYoungest(user);
