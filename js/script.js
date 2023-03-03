f0e18ff8692f8a060212d4639e0b891a3737d4a5
OnlineNomad <singularity123321@gmail.com>
2023-03-03 15:07:19 +0400

    Add fixes

--- js/script.js
+++ js/script.js
@@ -29,9 +29,10 @@ const b = {
 function findYoungest(user) {
     user.forEach(function(item){
         if (item.age <= 21){
-            console.log(`${item.name}`)
+            console.log(`youngest one is: ${item.name}`)
         }
     })
+
 }
 
 function createNewUser(user) {
@@ -39,18 +40,20 @@ function createNewUser(user) {
     return newUser;
 }
 
-console.log(user2);
-console.log(createNewUser(user2));
+console.log('old user: ', user2);
+console.log('new user: ', createNewUser(user2));
 findYoungest(user);
 
 while (true) {
-    a.rollDice();
-    b.rollDice();
-    if (a.rollDice() == 3) {
-        console.log(a.name + " Wins with " + a.triesA + " tries");
+    let tmprry = a.rollDice();
+    let tmprry2 = b.rollDice();
+    console.log(a.name, ' Rolled: ', tmprry);
+    console.log(b.name, ' Rolled: ', tmprry2);
+    if (tmprry === 3) {
+        console.log(a.name + " Wins with " + parseInt(a.triesA) + " tries");
         break;
-    }else if (b.rollDice() == 3) {
-        console.log(b.name + " Wins with " + b.triesB + " tries");
+    }else if (tmprry2 === 3) {
+        console.log(b.name + " Wins with " + parseInt(b.triesB) + " tries");
         break;
         
     }
